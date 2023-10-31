8844373772c8725621aee2ed10800c457e216132
William Pereira <wil.scp@gmail.com>
2023-10-31 12:21:05 -0300

    feat: create service and controller

---
+++ src/main/java/com/example/demo/controller/EstudanteController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Estudante;
+import com.example.demo.service.EstudanteService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("estudantes")
+@AllArgsConstructor
+public class EstudanteController {
+	
+	private EstudanteService estudanteService;
+	
+	@GetMapping("/{id}")
+	public  ResponseEntity<Estudante> buscarEstudante(@PathVariable Long id) {
+		return estudanteService.buscarEstudante(id);
+	}
+	
+	@GetMapping
+	public List<Estudante> buscarTodosEstudante() {
+		return estudanteService.buscarTodosEstudante();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante) {
+		return estudanteService.cadastrarEstudante(estudante);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Estudante> atualizarEstudante(@PathVariable Long id, @RequestBody Estudante estudante) {
+		return estudanteService.atualizarEstudante(estudante);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removerEstudante(@PathVariable Long id) {
+		return estudanteService.removerEstudante(id);
+	}
+}
--- src/main/java/com/example/demo/service/EstudanteService.java
+++ src/main/java/com/example/demo/service/EstudanteService.java
@@ -7,14 +7,16 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Estudante;
 
+@Service
 public class EstudanteService {
 
 	private static Map<Long, Estudante> listaEstudantes = new HashMap<>();
 	
-	private ResponseEntity<Estudante> buscarEstudante(Long id) {
+	public  ResponseEntity<Estudante> buscarEstudante(Long id) {
 		Estudante estudante = listaEstudantes.get(id);
 		if(estudante == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
@@ -22,19 +24,16 @@ public class EstudanteService {
 		return ResponseEntity.status(HttpStatus.OK).body(estudante);
 	}
 	
-	private List<Estudante> buscarTodosEstudante() {
+	public List<Estudante> buscarTodosEstudante() {
 		return new ArrayList<>(listaEstudantes.values());
 	}
 	
-	private ResponseEntity<Estudante> cadastrarEstudante(Estudante estudante) {
-		Estudante estudanteEncontrado = listaEstudantes.get(estudante.getId());
-		if(estudanteEncontrado == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(estudanteEncontrado);
+	public ResponseEntity<Estudante> cadastrarEstudante(Estudante estudante) {
+		listaEstudantes.put(estudante.getId(), estudante);
+		return ResponseEntity.status(HttpStatus.OK).body(estudante);
 	}
 	
-	private ResponseEntity<Estudante> atualizarEstudante(Estudante estudante) {
+	public ResponseEntity<Estudante> atualizarEstudante(Estudante estudante) {
 		Estudante estudanteEncontrado = listaEstudantes.get(estudante.getId());
 		if(estudanteEncontrado == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
@@ -43,7 +42,7 @@ public class EstudanteService {
 		return ResponseEntity.status(HttpStatus.OK).body(estudanteEncontrado);
 	}
 	
-	private ResponseEntity<String> deletarEstudante(Long id) {
+	public ResponseEntity<String> removerEstudante(Long id) {
 		Estudante estudanteEncontrado = listaEstudantes.get(id);
 		if(estudanteEncontrado == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
